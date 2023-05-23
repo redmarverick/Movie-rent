758fbd2d359941e783cb04210495ec2d2d3a438a
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-05-22 18:19:52 -0700

    finish comment popup basic html

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,5 @@
 const btn = document.querySelectorAll('.movie-button mr-2');
-const modal = document.getElementById('popup-container');
+const modal = document.getElementsByClassName('popup-container');
 for (let i = 0; i < btn.length; i += 1) {
   btn[i].addEventListener('click', () => {
     modal.innerHTML = `
@@ -7,19 +7,18 @@ for (let i = 0; i < btn.length; i += 1) {
 
         <span class="close" ><img src="./img/close-icon.png" /></span >
         <h1>${data[i].title}</h1>
-        </div>
-        <ul>${data[i].technologies}</ul>
-        <div class="popup-div">
-              <img src="${data[i].image}"></img>
-              <div class="modal-bottom">
+    </div>
 
-            <p>${data[i].description}</p>
-            <div class="btn-div">
-                <button type="button" aria-label="project live demo" tabindex="-13"><a href="${data[i].liveDemoLink}">See live<img src="./img/live-demo.png" alt="live demo icon" /></a></button>
-                <button type="button" aria-label="project source code" tabindex="-14"><a href="${data[i].sourceCodeLink}">See source<img src="./img/github-button.png" alt="github icon" /></a></button>
-                </div>
-                </div>
-                </div>
+            <h2>Comments</h2>
+            <div class="comment-container">
+            </div>
+            <form action="https://formspree.io/f/mjvdabnn" method="POST" id="comment-form">
+            <label for="name" aria-label="name">name*</label>
+            <input aria-label="name" id="name" type="text" placeholder="Your name" maxlength="30" name="name" tabindex="16" required>
+            <label for="comments" aria-label="comments">comments*</label>
+            <textarea aria-label="comments" id="comments" placeholder="Enter comments here" maxlength="500" name="message" required tabindex="18"></textarea>
+            <button aria-label="submit" type="submit" value="Submit" tabindex="19">Get in touch</button>
+        </form>
     `;
     const span = document.getElementsByClassName('close')[0];
     modal.style.display = 'flex';
