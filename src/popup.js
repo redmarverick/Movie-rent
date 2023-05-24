9eb7358c1c84481d14aeb17aee83d42257b0e052
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-05-24 17:13:30 -0400

    change close icon classes

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,15 @@
-export const popupWins = () =>{
+export const popupWins = () => {
   const btn = document.querySelectorAll('.movie-button.mr-2');
   const modal = document.createElement('div');
   modal.className = 'popup-container';
 
   for (let i = 0; i < btn.length; i += 1) {
-    console.log("in loop[@!")
     btn[i].addEventListener('click', () => {
+
       modal.innerHTML = `
     <div class="modal-top">
 
-        <span class="close" ><img src="./img/close-icon.png" /></span >
+        <span class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"></span >
         <h1>title</h1>
     </div>
 
@@ -24,9 +24,13 @@ export const popupWins = () =>{
             <button aria-label="submit" type="submit" value="Submit" tabindex="19">Get in touch</button>
         </form>
     `;
-      const span = document.getElementsByClassName('close')[0];
+
+      console.log("start click span")
+      const span = document.querySelector('span');
       modal.style.display = 'flex';
       span.onclick = () => {
+
+        console.log("enter click span")
         modal.style.display = 'none';
         modal.innerHTML = '';
       };
