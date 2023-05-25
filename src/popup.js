d2ce7e173424f5fee9fa882340cbeb480e5150d0
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-05-24 22:18:06 -0400

    Change to foreach loop

--- src/popup.js
+++ src/popup.js
@@ -4,42 +4,40 @@ export const popupWins = (root) => {
   const modal = document.createElement('div');
   modal.className = 'popup-container';
 
-  for (let i = 0; i < btn.length; i += 1) {
-    btn[i].addEventListener('click', () => {
-
+  btn.forEach((button) => {
+    button.addEventListener('click', () => {
       modal.innerHTML = `
-    <div class="modal-top">
-
-        <span class="close-icon bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"></span >
-        <h1>title</h1>
-    </div>
-
-            <h2>Comments</h2>
-            <div class="comment-container">
-            </div>
-            <form method="POST" id="comment-form">
-            <label for="name" aria-label="name">name*</label>
-            <input aria-label="name" id="name" type="text" placeholder="Your name" maxlength="30" name="name" tabindex="16" required>
-            <label for="comments" aria-label="comments">comments*</label>
-            <textarea aria-label="comments" id="comments" placeholder="Enter comments here" maxlength="500" name="message" required tabindex="18"></textarea>
-            <button aria-label="submit" type="submit" value="Submit" tabindex="19">Get in touch</button>
+        <div class="modal-top">
+          <span class="close-icon bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"></span >
+          <h1>title</h1>
+        </div>
+        <h2>Comments</h2>
+        <div class="comment-container">
+        </div>
+        <form method="POST" id="comment-form">
+          <label for="name" aria-label="name">name*</label>
+          <input aria-label="name" id="name" type="text" placeholder="Your name" maxlength="30" name="name" tabindex="16" required>
+          <label for="comments" aria-label="comments">comments*</label>
+          <textarea aria-label="comments" id="comments" placeholder="Enter comments here" maxlength="500" name="message" required tabindex="18"></textarea>
+          <button aria-label="submit" type="submit" value="Submit" tabindex="19">Get in touch</button>
         </form>
-    `;
-
-      console.log("start click span")
+      `;
+  
+      console.log("start click span");
       const span = document.getElementsByClassName('close-icon');
-      console.log("found span")
-
+      console.log("found span");
+  
       modal.style.display = 'flex';
-      console.log("style span")
-
+      console.log("style span");
+  
       span.onclick = () => {
-        debugger
-        console.log("enter click span")
+        debugger;
+        console.log("enter click span");
         modal.style.display = 'none';
         modal.innerHTML = '';
       };
       root.appendChild(modal);
     });
-  }
+  });
+  
 }
