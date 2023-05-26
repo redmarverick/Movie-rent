ae6a77b2bd97d123aef77e2a9daa633717f9cd0b
Nylbert Victor <nyldrift@hotmail.com>
2023-05-26 12:15:53 -0300

    Bugs correction

--- src/popup.js
+++ src/popup.js
@@ -1,45 +1,39 @@
-import {postComment, getComment } from './api.js';
-
-
+import { postComment, getComment } from './api.js';
 
 export const popupWins = (root) => {
   const btn = document.querySelectorAll('.movie-button.mr-2');
-  const modal = document.createElement('div');
-  modal.className = 'popup-container';
 
   for (let i = 0; i < btn.length; i += 1) {
+    const modal = document.createElement('div');
+    modal.className = 'popup-container';
 
-      modal.innerHTML = `
+    modal.innerHTML = `
     <div class="modal-top">
-
-        <span class="close-icon bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"></span >
-        <h1>title</h1>
+      <span class="close-icon bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">×</span>
+      <h1 class="text-xl font-bold">Title</h1>
     </div>
+    <h2 class="text-lg font-bold mt-4">Comments</h2>
+    <div class="comment-container"></div>
+    <form method="POST" id="comment-form" class="mt-4">
+      <label for="name" aria-label="name" class="block font-bold">Name*</label>
+      <input aria-label="name" id="name" type="text" placeholder="Your name" maxlength="30" name="name" tabindex="16" required class="w-full p-2 border border-gray-300 rounded mb-2">
+      <label for="comments" aria-label="comments" class="block font-bold">Comments*</label>
+      <textarea aria-label="comments" id="comments" placeholder="Enter comments here" maxlength="500" name="message" required tabindex="18" class="w-full p-2 border border-gray-300 rounded mb-2"></textarea>
+      <button aria-label="submit" type="submit" value="Submit" tabindex="19" class="bg-indigo-500 text-white px-4 py-2 rounded font-bold">Comment</button>
+    </form>
+  `;
 
-            <h2>Comments</h2>
-            <div class="comment-container">
-            </div>
-            <form method="POST" id="comment-form">
-            <label for="name" aria-label="name">name*</label>
-            <input aria-label="name" id="name" type="text" placeholder="Your name" maxlength="30" name="name" tabindex="16" required>
-            <label for="comments" aria-label="comments">comments*</label>
-            <textarea aria-label="comments" id="comments" placeholder="Enter comments here" maxlength="500" name="message" required tabindex="18"></textarea>
-            <button aria-label="submit" type="submit" value="Submit" tabindex="19">Comment</button>
-        </form>
-    `;
+  root.appendChild(modal);
+  const span = modal.querySelector('.close-icon');
 
-    root.appendChild(modal);
-      const span = document.querySelector('.close-icon');
+  modal.style.display = 'flex';
 
-      modal.style.display = 'flex';
-
-      span.onclick = () => {
-        console.log("enter click span")
-        modal.style.display = 'none';
-        modal.innerHTML = '';
-      };
+  span.onclick = () => {
+    modal.style.display = 'none';
+    modal.innerHTML = '';
+  };
   }
-}
+};
 
 const createComment = () => {
   const form = document.getElementById('comment-form');
@@ -48,33 +42,36 @@ const createComment = () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const newName = formName.value;
-    const newComment = Number(formScore.value);
-    const comment_obj = {
+    const newComment = formComment.value;
+    const commentObj = {
       username: newName,
       comment: newComment,
     };
-    formName.value = '';
-    formComment.value = '';
-    postComment(comment_obj);
+    form.reset();
+    postComment(commentObj);
   });
 };
-
-createComment();
 
 const getComments = () => {
-  const form = document.getElementById('comment-form');
-  const commentList = document.getElementsByClassName('comment-container');
-  form.addEventListener('click', async (event) => {
-    event.preventDefault();
-    const refreshNewComment = await getComment();
-    commentList.innerHTML = ''; // clear the list before repopulating
-    refreshNewComment.sort((a, b) => a.index - b.index); // sort scores in descending order
-    refreshNewComment.forEach((rnc) => {
-      const newCommentx = document.createElement('li');
-      newScore.innerHTML = `${rnc.user}: ${rnc.score}`;
-      commentList.appendChild(newCommentx);
+  const commentList = document.querySelector('.comment-container');
+  const refreshComments = async () => {
+    const comments = await getComment();
+    commentList.innerHTML = '';
+    comments.sort((a, b) => a.index - b.index);
+    comments.forEach((comment) => {
+      const newComment = document.createElement('li');
+      newComment.innerHTML = `${comment.user}: ${comment.score}`;
+      commentList.appendChild(newComment);
     });
-  });
+  };
+
+  refreshComments();
+  document.getElementById('comment-form').addEventListener('click', refreshComments);
 };
 
-getComments();
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root'); // Replace 'root' with the actual ID of your root element
+  popupWins(rootElement);
+  createComment();
+  getComments();
+});
