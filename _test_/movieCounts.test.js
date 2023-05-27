976be8df0279b8e7966bee917cc248a7c95378bd
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-05-27 13:20:37 -0400

    fix eslint errors
    Co-Authored-By: Nylbert Victor <nyldrift@hotmail.com>

--- _test_/movieCounts.test.js
+++ _test_/movieCounts.test.js
@@ -4,11 +4,8 @@
 
 import countmovies from '../src/countMovies.js';
 
-
 describe('item count test', () => {
-  
-
-  beforeEach(() => { 
+  beforeEach(() => {
     document.body.innerHTML = `
     <div id="moviesContainer" class="flex flex-wrap gap-4 justify-center">
       <div id="item1" class="movie-card w-64 p-4 bg-gray-800 text-white rounded" style="display: flex; flex-flow: column;">
@@ -18,33 +15,17 @@ describe('item count test', () => {
   </div>
   `;
   });
-
-
   afterEach(() => {
-    document.body.innerHTML = ''; 
+    document.body.innerHTML = '';
   });
-
-
   // first test
   test('returns 4 if we have 4 movies in the movies container', () => {
-    const moveiItems = document.getElementById('moviesContainer');
-    console.log(moveiItems.textContent)
-    expect(countmovies()).toBe(4)
-    
+    expect(countmovies()).toBe(4);
   });
-
-
   // second test
   test('returns 0 when there are not movies in the container', () => {
-
     const moviesContainer = document.getElementById('moviesContainer');
     moviesContainer.innerHTML = '';
-
-    expect(moviesContainer.childElementCount).toBe(0)
-
-
+    expect(moviesContainer.childElementCount).toBe(0);
   });
-
-
-
 });
